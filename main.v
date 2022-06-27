7dff645c4101cc8367d88733498353e531e9c8bd
Tin LetHax <thipok17@gmail.com>
2022-06-27 19:21:42 +0700

    [W.I.P]Added more feature.
    - added support for the trigger functionality.
    - corrected cmd_max number.
    - initial block is no longer used (I'm kinda dumb, don't know that it won't work with iCE40).
    - added 500k 200k and 100k sps to command decoder.
    - improved BRAM state machine.
    
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- main.v
+++ main.v
@@ -69,7 +69,7 @@ reg [1:0]cmd_byte_n = 0;
 reg [2:0]bram_steps = 0; 
 
 // params for command and flags
-parameter cap_size = 8192 - 1;
+parameter cap_size = 8192 - 1;// capture size is 8192 bytes, stop the capture when address is reached 'h8191.
 reg cmd_max = 0;
 // commands 
 // 1 byte cmd 
@@ -81,8 +81,9 @@ parameter cmd_metadat = 8'h04;
 // 5 bytes cmd
 parameter cmd_settrigmask = 8'hc0;
 parameter cmd_settrigsens = 8'hc1;
+parameter cmd_settrigconf = 8'hc2;
 parameter cmd_setdiv = 8'h80;
-
+parameter cmd_setsamcnt = 8'h81;// set read count and delay count (in unit of sample/4), this probably used for setting pre-post trigger ratio.
 reg [23:0]cap_div = 0;
 
 // Dividers for each sample rate
@@ -115,6 +116,24 @@ reg [7:0]cap_limit = 0;
 // sensi : [7:0, CH7, CH6, CH5, CH4, CH3, CH2, CH1, CH0].
 reg [7:0]trig_mask = 0;
 reg [7:0]trig_sens = 0;
+// trigger address register stores the memory address of the input data that match trigger setting.
+reg [12:0]trig_addr = 0;
+// trigger end address store the address of "what suppose to be" last input data captured. This make sure that we don't override previous capture that
+// was a part of pre-trigger. 
+reg [12:0]trig_end_addr = 0;
+
+// check for input to match trigger condition.
+wire trig_match;
+assign trig_match = !((CAP ^ trig_sens) & trig_mask);
+
+// store how many sample to read back from memory and how many sample to capture after trigger is fired.
+reg [15:0]read_size = 0;
+reg [15:0]delay_size = 0;
+// from 2 regs above. read_size - delay_size == pre trigger sample count (size).
+reg [15:0]trig_pre_size = 0;
+
+// state machine for trigger
+reg [1:0]trig_fsm = 0;
 
 // INT flag
 // Interrupt flag set to 1 after capture is done.
@@ -148,6 +167,7 @@ uart_rx #(clk_freq, baud) urx1 (
 );
 
 /* instantiate the tx1 module */
+reg TX_sel = 0;// select between 0 Captured data to TX or 1 ID report to TX.
 reg tx1_start;
 wire txbusy;
 wire tx1_data;
@@ -161,72 +181,10 @@ uart_tx #(clk_freq, baud) utx1 (
 	.tx_busy(txbusy)
 );	
 	
-
 // metadat and id reports.
 reg [7:0] mdtd [31:0];
 reg [7:0] id [3:0];
 reg [4:0] id_cnt = 0;
-reg TX_sel = 0;// select between 0 Captured data to TX or 1 ID report to TX.
-
-initial begin
-	// "1ALS" report
-	id[0] <= 'h31;// 1
-	id[1] <= 'h41;// A
-	id[2] <= 'h4C;// L
-	id[3] <= 'h53;// S
-	
-	// Name report
-	mdtd[0] <= 'h01;
-	// Name : iCECapture
-	mdtd[1] <= 'h69;// i
-	mdtd[2] <= 'h43;// C
-	mdtd[3] <= 'h45;// E
-	mdtd[4] <= 'h43;// C
-	mdtd[5] <= 'h61;// a
-	mdtd[6] <= 'h70;// p
-	mdtd[7] <= 'h74;// t
-	mdtd[8] <= 'h75;// u
-	mdtd[9] <= 'h72;// r
-	mdtd[10] <= 'h65;// e
-	
-	mdtd[11] <= 'h00;// end marker 
-	
-	// Version report
-	mdtd[12] <= 'h02;
-	// Version : 0.1
-	mdtd[13] <= 'h30;// 0
-	mdtd[14] <= 'h2e;// .
-	mdtd[15] <= 'h31;// 1
-	
-	mdtd[16] <= 'h00;// end marker 
-	
-	// Sample memory depth (8KiB).
-	mdtd[17] <= 'h21;
-	// Sample mem depth (uint32_t) MSB first
-	mdtd[18] <= 'h00;
-	mdtd[19] <= 'h00;
-	mdtd[20] <= 'h20;
-	mdtd[21] <= 'h00;
-	
-	// Sample rate, Maximum is 48MSa/s.
-	mdtd[22] <= 'h23;
-	mdtd[23] <= 'h02;
-	mdtd[24] <= 'hdc;
-	mdtd[25] <= 'h6c;
-	mdtd[26] <= 'h00;
-	
-	// Input channel number report (8 channels)
-	mdtd[27] <= 'h40;
-	mdtd[28] <= 'd8;// we have 8 channels 
-	
-	// Protocol version (2)
-	mdtd[29] <= 'h41;
-	mdtd[30] <= 'h02;
-	
-	// End marking
-	mdtd[31] <= 'h00;
-
-end
 	
 // main FSM.
 
@@ -235,16 +193,77 @@ reg [2:0]main_fsm_cnt = 0;
 always@(posedge CAP_CLK)begin
 
 	case(main_fsm_cnt)
-	0: begin// Wait for RX
+	0: begin // Initialize default values.
+		// "1ALS" report
+		id[0] <= 'h31;// 1
+		id[1] <= 'h41;// A
+		id[2] <= 'h4C;// L
+		id[3] <= 'h53;// S
+		
+		// Name report
+		mdtd[0] <= 'h01;
+		// Name : iCECapture
+		mdtd[1] <= 'h69;// i
+		mdtd[2] <= 'h43;// C
+		mdtd[3] <= 'h45;// E
+		mdtd[4] <= 'h43;// C
+		mdtd[5] <= 'h61;// a
+		mdtd[6] <= 'h70;// p
+		mdtd[7] <= 'h74;// t
+		mdtd[8] <= 'h75;// u
+		mdtd[9] <= 'h72;// r
+		mdtd[10] <= 'h65;// e
+		
+		mdtd[11] <= 'h00;// end marker 
+		
+		// Version report
+		mdtd[12] <= 'h02;
+		// Version : 0.1
+		mdtd[13] <= 'h30;// 0
+		mdtd[14] <= 'h2e;// .
+		mdtd[15] <= 'h31;// 1
+		
+		mdtd[16] <= 'h00;// end marker 
+		
+		// Sample memory depth (8KiB).
+		mdtd[17] <= 'h21;
+		// Sample mem depth (uint32_t) MSB first
+		mdtd[18] <= 'h00;
+		mdtd[19] <= 'h00;
+		mdtd[20] <= 'h20;
+		mdtd[21] <= 'h00;
+		
+		// Sample rate, Maximum is 48MSa/s.
+		mdtd[22] <= 'h23;
+		mdtd[23] <= 'h02;
+		mdtd[24] <= 'hdc;
+		mdtd[25] <= 'h6c;
+		mdtd[26] <= 'h00;
+		
+		// Input channel number report (8 channels)
+		mdtd[27] <= 'h40;
+		mdtd[28] <= 'd8;// we have 8 channels 
+		
+		// Protocol version (2)
+		mdtd[29] <= 'h41;
+		mdtd[30] <= 'h02;
+		
+		// End marking
+		mdtd[31] <= 'h00;
+		
+		main_fsm_cnt <= 1;
+	end
+	
+	1: begin// Wait for RX
 	
 		if(rxready) begin
 			// copy data from UART to command buffer.
-			if(cmd_byte_n <= cmd_max) 
+			if(cmd_byte_n == cmd_max) 
 				cmd_byte_n <= 0;
 			else
 				cmd_byte_n <= cmd_byte_n + 1;
 				
-			cmdbuf[cmd_byte_n] <= rx1_data;// store all 4 bytes into command buffer, auto-increment.
+			cmdbuf[cmd_byte_n] <= rx1_data;// store all 1 or 5 bytes into command buffer, auto-increment.
 
 			// command length 
 			case(cmdbuf[0])
@@ -256,12 +275,13 @@ always@(posedge CAP_CLK)begin
 				// 5 bytes command variants. 
 				cmd_setdiv,
 				cmd_settrigmask,
-				cmd_settrigsens: cmd_max <= 4;
+				cmd_settrigsens,
+				cmd_setsamcnt: cmd_max <= 4;
 				
 				default: cmd_max <= 0;
 			endcase 
 			
-			if (cmd_byte_n == cmd_max) begin // received all 5 bytes, then decoder begins. 
+			if (cmd_byte_n == cmd_max) begin // received 1 or 5 bytes, then decoder begins. 
 
 				case(cmdbuf[0])
 						// Flag: Capture. Capture start immediately after this command is decoded.
@@ -272,9 +292,13 @@ always@(posedge CAP_CLK)begin
 								cap_24m,
 								cap_12m,
 								cap_6m,
-								cap_2m: begin 
-									main_fsm_cnt <= 1; // move to Start Capture stage.
+								cap_2m,
+								cap_500k,
+								cap_200k,
+								cap_100k: begin 
+									main_fsm_cnt <= 2; // move to Start Capture stage.
 									cap_wr_ce <= 1;// Enable Mem write.
+									cap_wr_addr <= 0;
 								end
 								default: begin
 									main_fsm_cnt <= main_fsm_cnt;
@@ -330,25 +354,34 @@ always@(posedge CAP_CLK)begin
 							
 						cmd_metadat: begin // Report SUMP Metadata to OLS.
 							TX_sel <= 1;
-							main_fsm_cnt <= 4;
+							main_fsm_cnt <= 5;
 						end
 							
 						cmd_id: begin // OLS expect "1ALS" send over UART.
-							TX_sel <=1;
-							main_fsm_cnt <= 5;
+							TX_sel <= 1;
+							main_fsm_cnt <= 6;
 						end
 							
 						// Flag: Trigger mask setup, this will select which pin we want to be used as trigger
 						// Format [0:cmd][1:LSB][2:LWRMID][3:HGRMID][4:MSB] but we only care [1:LSB] since we only have 8 channel inputs
 						cmd_settrigmask: begin
 								trig_mask <= cmdbuf[1];
-							end
+						end
 						
 						// Flag: Trigger sensitivity setup, whether triggers when falling or rising edge.
 						// Format [0:cmd][1:LSB][2:LWRMID][3:HGRMID][4:MSB], again we only care [1:LSB] since we only have 8 channel inputs
 						cmd_settrigsens: begin
 								trig_sens <= cmdbuf[1];
-							end
+						end
+						
+						// Flag: Set Read count (how many sample to read back) and Delay count (how many sample to capture after the trigger is fired). All in unit of samples/4
+						// Format [0:cmd][1:readcnt_LSB][2:readcnt_MSB][3:dlycnt_LSB][4:dlycnt_MSB]
+						cmd_setsamcnt: begin
+								// convert data back from sample/4 to sample unit.
+								read_size <= {cmdbuf[2], cmdbuf[1]} << 2; 
+								delay_size <= {cmdbuf[4], cmdbuf[3]} << 2;
+								trig_pre_size <= read_size - delay_size;// calculate sample size of pre trigger.
+						end
 						
 						// Flag: set divider, this will select sample rate.
 						// Format [0:cmd][1:LSB][2:MID][3:MSB][4:IGNORED]
@@ -356,8 +389,7 @@ always@(posedge CAP_CLK)begin
 								cap_div[7:0] <= cmdbuf[1];
 								cap_div[15:8] <= cmdbuf[2];
 								cap_div[23:16] <= cmdbuf[3];
-							end
-					
+						end
 					
 				endcase
 			
@@ -367,81 +399,120 @@ always@(posedge CAP_CLK)begin
 	
 	end
 	
-	1: begin// Start Capture
+	2: begin// Start Capture
 	
 		cap_counter <= cap_counter + 1;
 		if(cap_counter == cap_limit) 
 			cap_counter <= 0;
-
-		if(cap_wr_addr == cap_size)begin// Capture done. move to TX stage.
-			cap_wr_ce <= 0;
-			main_fsm_cnt <= 2;
-		end
-		else begin// Still capturing. 
+			
+		if(trig_mask == 8'h00) begin// if somehow entered trigger mode but with trigger mask set all input to 0, goes back to normal capture mode.
 		
-			if(cap_limit == 0) begin // 48MSa/s running as System clock.
-				cap_wr_addr <= cap_wr_addr + 1;
-			end
-			else begin// sample rate less than 48MSa/s capture depends on free-running counter.
-				if(cap_counter == 0) begin 
-					cap_wr_addr <= cap_wr_addr + 1;
-					cap_wr_ce <= 1;
-				end
-				else
-					cap_wr_ce <= 0;
-
+			if(cap_wr_addr == cap_size)begin// Capture done. move to TX stage.
+				cap_wr_ce <= 0;
+				cap_wr_addr <= 0;
+				main_fsm_cnt <= 3;
+				trig_end_addr <= cap_size;
 			end
 			
-		main_fsm_cnt <= main_fsm_cnt;
+		end
+		else begin
+		
+			case(trig_fsm)// Capture with trigger.
+			0: begin// wait for trigger to acquire trigger address.
+				if(trig_match) begin
+					trig_addr <= cap_wr_addr;
+					trig_end_addr <= cap_wr_addr + delay_size;
+					trig_fsm <= 1;
+				end
+					
+			end
+			
+			1: begin// wait until post trigger input data is written to the memory. 
+				if(cap_wr_addr == trig_end_addr)begin
+					// reached the end of "trigger" memory. Stop capture.
+					cap_wr_ce <= 0;
+					cap_wr_addr <= 0;
+					main_fsm_cnt <= 3;
+					trig_fsm <= 0;
+					cap_rd_addr <= trig_end_addr + 1;// start sending the pre capture. Then counter rolls over back to 0 and end at trig_end_addr.
+				end	
+				
+			end
+			
+			endcase
+			
+		end
+
+		case(cap_limit)// BRAM writing address up counter.
+		0: cap_wr_addr <= cap_wr_addr + 1;
+		default : begin
+			if(cap_counter == 0) begin 
+				cap_wr_addr <= cap_wr_addr + 1;
+				cap_wr_ce <= 1;
+			end
+			else
+				cap_wr_ce <= 0;
 		
 		end
+		
+		endcase
+
+			
+		main_fsm_cnt <= main_fsm_cnt;
 			
 	end
 	
-	2: begin// Start TX to host.
+	3: begin// Start TX to host.
 		
 		INT_flag <= 1;
+		TX_sel <= 0;
 		
 		if(txbusy)begin // if busy, just wait until transmit done.
 			tx1_start <= 0;
 		end
 		else begin // send data back to PC
 		
-			if(cap_rd_addr == cap_size) begin
-				cap_rd_ce <= 0;// diable BRAM read 
-				main_fsm_cnt <= 3;
+			if(cap_rd_addr == trig_end_addr) begin
+				cap_rd_ce <= 0;// disble BRAM read 
+				cap_rd_addr <= 0; // reset BRAM read address to 0.
+				main_fsm_cnt <= 4;
 				INT_flag <= 0;
 				bram_steps <= 0;
 			end
 			else begin
-				
-				main_fsm_cnt <= main_fsm_cnt;
-				
-				// enable BRAM read back 
-				cap_rd_ce <= 1; 
-			
+
 				// finite state machine for BRAM read back.
 				case(bram_steps)
-				0:	begin// write address to ram
-					cap_rd_addr <= cap_rd_addr + 1;
+				0:	begin// enable bram write and write first address to bram.
+					// enable BRAM read back 
+					cap_rd_ce <= 1; 
+					cap_rd_addr <= 0;
 					bram_steps <= 1;// move to next step, wait
-					end
-				1: /*do nothing*/ bram_steps <= 2;
+					
+				end
+				1: begin
+					// write next address and read data from previous address on next clock cycle.
+					cap_rd_addr <= cap_rd_addr + 1;
+					bram_steps <= 2;
+				end
+				
 				2: begin // read from ram and write to UART TX
 					//tx1_data <= cap2host;
 					tx1_start <= 1;
-					bram_steps <= 0;
+					bram_steps <= 1;
 					end
 				endcase
+				
+				main_fsm_cnt <= main_fsm_cnt;
 				
 			end
 		end//if(tx1_busy)
 	
 	end
 	
-	3: main_fsm_cnt <= 0;
+	4: main_fsm_cnt <= 1;
 	
-	4:begin// Special stage, used for metadata report. 
+	5:begin// Special stage, used for metadata report. 
 	
 		if(txbusy)begin // if busy, just wait until transmit done.
 			tx1_start <= 0;
@@ -450,7 +521,7 @@ always@(posedge CAP_CLK)begin
 			id_cnt <= id_cnt + 1;
 			if(id_cnt == 31)begin
 				id_cnt <= 0;
-				main_fsm_cnt <= 0;
+				main_fsm_cnt <= 1;
 				TX_sel <= 0;
 				tx1_start <= 0;
 			end
@@ -464,7 +535,7 @@ always@(posedge CAP_CLK)begin
 	
 	end
 	
-	5:begin// Special stage, used for ID report "1ALS".
+	6:begin// Special stage, used for ID report "1ALS".
 	
 		if(txbusy)begin // if busy, just wait until transmit done.
 			tx1_start <= 0;
@@ -473,7 +544,7 @@ always@(posedge CAP_CLK)begin
 			id_cnt <= id_cnt + 1;
 			if(id_cnt == 3)begin
 				id_cnt <= 0;
-				main_fsm_cnt <= 0;
+				main_fsm_cnt <= 1;
 				TX_sel <= 0;
 				tx1_start <= 0;
 			end
@@ -491,95 +562,19 @@ always@(posedge CAP_CLK)begin
 end	
 
 endmodule
-/* report ID example 
-
-reg [7:0]mdtd[31:0];
-
-initial begin
-	// Name report
-	mdtd[0] <= 'h01;
-	// Name : iCECapture
-	mdtd[1] <= 'h69;// i
-	mdtd[2] <= 'h43;// C
-	mdtd[3] <= 'h45;// E
-	mdtd[4] <= 'h43;// C
-	mdtd[5] <= 'h61;// a
-	mdtd[6] <= 'h70;// p
-	mdtd[7] <= 'h74;// t
-	mdtd[8] <= 'h75;// u
-	mdtd[9] <= 'h72;// r
-	mdtd[10] <= 'h65;// e
-	
-	mdtd[11] <= 'h00;// end marker 
-	
-	// Version report
-	mdtd[12] <= 'h02;
-	// Version : 0.1
-	mdtd[13] <= 'h30;// 0
-	mdtd[14] <= 'h2e;// .
-	mdtd[15] <= 'h31;// 1
-	
-	mdtd[16] <= 'h00;// end marker 
-	
-	// Sample memory depth (8KiB).
-	mdtd[17] <= 'h21;
-	// Sample mem depth (uint32_t) MSB first
-	mdtd[18] <= 'h00;
-	mdtd[19] <= 'h00;
-	mdtd[20] <= 'h20;
-	mdtd[21] <= 'h00;
-	
-	// Sample rate, Maximum is 48MSa/s.
-	mdtd[22] <= 'h23;
-	mdtd[23] <= 'h02;
-	mdtd[24] <= 'hdc;
-	mdtd[25] <= 'h6c;
-	mdtd[26] <= 'h00;
-	
-	// Input channel number report (8 channels)
-	mdtd[27] <= 'h40;
-	mdtd[28] <= 'd8;// we have 8 channels 
-	
-	// Protocol version (2)
-	mdtd[29] <= 'h41;
-	mdtd[30] <= 'h02;
-	
-	// End marking
-	mdtd[31] <= 'h00;
-
-end
 
 
-	usart_write(0x01);// name report
-	prntf("ComeNCapture");
-	usart_write(0x00);// end marker
-
-	usart_write(0x02);// version report
-	prntf("2.0");
-	usart_write(0x00);// end marker
-
-	// Sample memory size (16Kbytes)
-	usart_write(0x21);// Sample size report in uint32_t format 
-	usart_write(0x00);
-	usart_write(0x00);
-	usart_write(0x3F);
-	usart_write(0xFF);
-
-	// Sample rate, assuming it's 8MHz
-	usart_write(0x23);// Sample rate report in uint32_t format 
-	usart_write(0x00);
-	usart_write(0x7A);
-	usart_write(0x12);
-	usart_write(0x00);
-
-	// we have 5 usable channels 
-	usart_write(0x40);// report channel number
-	usart_write(0x05);// 5 channels
-	
-	// protocol version is  2 
-	usart_write(0x41);// report protocol version
-	usart_write(0x02);
-
-	usart_write(0x00);// end marking
-
+/* Pre / Post trigger calculation algorithm. 
+	input from pc -> Read count, Delay count (all in sample/4 unit).
+	Example Read count == 1 -> 4 sample.
+1. Convert sample/4 into sample by multiplying with 4
+	read_size <= read_count << 2;// m << n means m times 2 to the n.
+	delay_size <= delay_count << 2;
+2. calculate pre trigger sample count
+	tirg_pre_size <= read_size - delay_size;
+3. calculate the end address of trigger capture.
+	trig_addr <= cap_wr_addr;// trig_addr will be equal to writing address when trigger is fired. 
+	trig_end_addr <= cap_wr_addr - trig_addr;
+4. wait until trig_end_addr equal to the end address calculate with triggered address plus delay_size.
+	if(trig_end_addr == (trig_addr + delay_size))
 */
